b26860c1ddbffc2c1cea8e36ac49ec7335293904
Wxlfie <47796829+Wxlfie@users.noreply.github.com>
2020-10-29 14:46:59 +0000

    Module 2
    First revision

---
+++ GradeBook/GradeBooks/RankedGradeBooks.cs
@@ -0,0 +1,13 @@
+﻿using GradeBook.Enums;
+using GradeBook.GradeBooks;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBooks : BaseGradeBook
+    {
+        public RankedGradeBooks(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
