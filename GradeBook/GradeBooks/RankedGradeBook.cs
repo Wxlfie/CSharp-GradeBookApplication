fa0bb61fbb9b6eb165620892b3d8a476ad5998c2
Wxlfie <47796829+Wxlfie@users.noreply.github.com>
2020-11-02 14:04:44 +0000

    Mod 3 First Revision
    Adding code for different grades

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using GradeBook.Enums;
 using GradeBook.GradeBooks;
+using System;
 
 namespace GradeBook.GradeBooks
 {
@@ -8,6 +9,30 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5) 
+            {
+                throw new InvalidOperationException();
+            }
+
+            if(averageGrade > 80)
+            {
+                return 'A';
+            }
+
+            if (averageGrade > 61 && averageGrade < 79)
+            {
+                return 'B';
+            }
+
+            if (averageGrade > 41 && averageGrade < 59)
+            {
+                return 'C';
+            }
+            return 'F';
         }
     }
 }
